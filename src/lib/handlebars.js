9f3db3b04c89beda06c51b675589077fd9ad19d5
Hiroshi <ebruno.hiroshi@gmail.com>
2022-05-21 16:41:35 -0500

    Se arregla diferencia horaria con base de datos en timeago.js

--- src/lib/handlebars.js
+++ src/lib/handlebars.js
@@ -2,7 +2,7 @@ const {format} = require('timeago.js');
 
 const helpers = {
     timeago: function(timestamp){
-        return format(timestamp)
+        return format(timestamp - 5 * 1000 * 3600)
     }
 }
 
